e3b0e8f73d9e8ee84abcce634eecfb50b0eb4cc1
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-12-25 15:12:31 +0100

    create AppImages

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,8 @@
+/// [AppImages] is a class that contains all the path of  images
+class AppImages {
+  /// [logo] is a variable that contains the path of the logo image
+  static const String logo = 'assets/svg/logo.svg';
+
+  /// [iconGame] is a variable that contains the path of the icon game
+  static const String iconGame = 'assets/svg/icon_game.svg';
+}
