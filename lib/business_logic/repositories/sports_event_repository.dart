4684ada531abd38694467ea2f2c8136ca08c38b8
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-12-26 00:51:24 +0100

    create SportsEventRepository

---
+++ lib/business_logic/repositories/sports_event_repository.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:sports_event_app/core/service_locator.dart';
+import 'package:sports_event_app/data/model/sports_event/sports_event_model.dart';
+import 'package:sports_event_app/data/providers/sports_event_provider.dart';
+import 'package:sports_event_app/utils/app_images.dart';
+import 'package:sports_event_app/utils/constants.dart';
+
+/// [SportsEventRepository] is a class that provides [SportsEventModel] object.
+class SportsEventRepository {
+  final SportsEventProvider _sportsEventProvider = getIt<SportsEventProvider>();
+
+  /// Function that gets  the [SportsEventModel] objects from data layer.
+  Future<List<SportsEventModel>> getEvents() async {
+    try {
+      return await _sportsEventProvider.fetchEvents();
+    } catch (e) {
+      debugPrint('Error in repository while fetching events: $e');
+      return [
+        SportsEventModel(
+          iconUrl: AppImages.iconGameNull,
+          league: tNullIndependentLeague,
+          teams: tTextNullTeams,
+          sportType: tTextNullSportType,
+          dateStarting: tTextNullDateStarting,
+          timeStarting: tTextNullTimeStarting,
+        ),
+      ];
+    }
+  }
+}
