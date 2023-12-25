ee0ad8e518ddf5d1ad65bb7ce985da2043b6ec5f
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-12-25 15:12:49 +0100

    create constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: public_member_api_docs
+
+// Spacings and Paddings
+const double tSpaceEighth = 1;
+const double tSpaceQuarter = 2;
+const double tSpaceHalf = 4;
+const double tSpaceSmall = 6;
+const double tSpaceBase = 8;
+const double tSpace = 12;
+const double tSpaceDouble = 16;
+const double tSpaceVerticalPadding = 24;
+const double tSpaceHorizontalPadding = 20;
+const double tSpaceFourfold = 32;
+const double tSpaceHuge = 24;
+
+//App Bar
+const double tHeightAppBar = 44;
+const double tHeightAppBarLogo = 24;
+const double tWidthAppBarLogo = 44;
+
+// Font Sizes
+const double tFontSizeSmall = 10;
+const double tFontSizeMedium = 14;
+const double tFontSizeLarge = 20;
+const double tFontHeight = 1.2;
+
+// Font Family
+const String tTextFontFamilyRegular = 'Inter-Regular';
