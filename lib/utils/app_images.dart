d7f7e725c3a40751571d8182108541ee8847657d
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-12-26 00:46:47 +0100

    add iconPaths

--- lib/utils/app_images.dart
+++ lib/utils/app_images.dart
@@ -3,6 +3,18 @@ class AppImages {
   /// [logo] is a variable that contains the path of the logo image
   static const String logo = 'assets/svg/logo.svg';
 
-  /// [iconGame] is a variable that contains the path of the icon game
-  static const String iconGame = 'assets/svg/icon_game.svg';
+  /// [iconGameNull] is a variable that contains the path of the icon game
+  static const String iconGameNull = 'assets/svg/icon_game_null.svg';
+
+  /// [iconBasketball] is a variable that contains the path of the icon basketball
+  static const String iconBasketball = 'assets/svg/icon_basketball.svg';
+
+  /// [iconFootball] is a variable that contains the path of the icon football
+  static const String iconFootball = 'assets/svg/icon_football.svg';
+
+  /// [iconTennis] is a variable that contains the path of the icon tennis
+  static const String iconTennis = 'assets/svg/icon_tennis.svg';
+
+  /// [iconVolleyball] is a variable that contains the path of the icon volleyball
+  static const String iconVolleyball = 'assets/svg/icon_volleyball.svg';
 }
