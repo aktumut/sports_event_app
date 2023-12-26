e7bd2c68fcbea2a78bfb3e0a843248397ba58432
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-12-26 22:54:25 +0100

    add automaticallyImplyLeading

--- lib/presentation/widgets/custom_app_bar.dart
+++ lib/presentation/widgets/custom_app_bar.dart
@@ -14,6 +14,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
     return PreferredSize(
       preferredSize: preferredSize,
       child: AppBar(
+        automaticallyImplyLeading: false,
         title: const LogoWidget(),
       ),
     );
