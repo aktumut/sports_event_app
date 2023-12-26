b7cb7c9cb27f73484f946d636b526897f08bc120
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-12-26 22:54:09 +0100

    move event_card.dart

--- lib/presentation/pages/events_page/events_page_widgets/event_card.dart
+++ lib/presentation/pages/events_page/events_page_widgets/event_card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sports_event_app/data/model/sports_event/sports_event_model.dart';
-import 'package:sports_event_app/presentation/widgets/event_card_info.dart';
-import 'package:sports_event_app/utils/colors.dart';
+import 'package:sports_event_app/presentation/pages/events_page/events_page_widgets/event_card_info.dart';
 import 'package:sports_event_app/utils/constants.dart';
 
 /// [EventCard] is a widget that displays the card of an event.
@@ -24,8 +23,8 @@ class EventCard extends StatelessWidget {
           event: event,
         ),
         const Divider(
-          color: tColorDivider,
           thickness: tSpaceEighth,
+          height: tSpaceHalf,
           indent: tSpaceHorizontalPadding,
           endIndent: tSpaceHorizontalPadding,
         ),
